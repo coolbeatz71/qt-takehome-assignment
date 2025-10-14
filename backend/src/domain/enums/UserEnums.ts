4f8b6a4561e79f2755a204b958e1248949792a9c
coolbeatz71 <sigmacool@gmail.com>
2025-10-14 18:00:00 +0200

    feat: implement UserRole and UserStatus enums

---
+++ backend/src/domain/enums/UserEnums.ts
@@ -0,0 +1,18 @@
+/**
+ * User Role Enum
+ * Defines the possible roles a user can have in the system
+ */
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+  GUEST = 'guest'
+}
+
+/**
+ * User Status Enum
+ * Defines the possible statuses a user can have
+ */
+export enum UserStatus {
+  ACTIVE = 'active',
+  INACTIVE = 'inactive'
+}
