c0d2377bad66e63e376d31446d0eb8a1b7a4170d
coolbeatz71 <sigmacool@gmail.com>
2025-10-17 11:00:00 +0200

    feat: implement user command use cases

---
+++ frontend/src/features/users/application/usecases/commands/CreateUser.usecase.ts
@@ -0,0 +1,23 @@
+import type { IUserRepository } from '../../repositories/IUserRepository';
+import type { User } from '../../../domain/entities/User.entity';
+import type { CreateUserDTO } from '../../../domain/dto/CreateUserDTO';
+
+/**
+ * Create user use case for creating a new user in the system
+ */
+export class CreateUserUseCase {
+  /**
+   * Creates a new CreateUserUseCase instance
+   * @param {IUserRepository} userRepository - User repository instance
+   */
+  constructor(private readonly userRepository: IUserRepository) {}
+
+  /**
+   * Execute the use case to create a new user
+   * @param {CreateUserDTO} data - User creation data
+   * @returns {Promise<User>} Created user entity
+   */
+  async execute(data: CreateUserDTO): Promise<User> {
+    return await this.userRepository.create(data);
+  }
+}
---
+++ frontend/src/features/users/application/usecases/commands/UpdateUser.usecase.ts
@@ -0,0 +1,24 @@
+import type { IUserRepository } from '../../repositories/IUserRepository';
+import type { User } from '../../../domain/entities/User.entity';
+import type { UpdateUserDTO } from '../../../domain/dto/UpdateUserDTO';
+
+/**
+ * Update user use case for updating an existing user
+ */
+export class UpdateUserUseCase {
+  /**
+   * Creates a new UpdateUserUseCase instance
+   * @param {IUserRepository} userRepository - User repository instance
+   */
+  constructor(private readonly userRepository: IUserRepository) {}
+
+  /**
+   * Execute the use case to update a user
+   * @param {number} id - User ID
+   * @param {UpdateUserDTO} data - User update data
+   * @returns {Promise<User>} Updated user entity
+   */
+  async execute(id: number, data: UpdateUserDTO): Promise<User> {
+    return await this.userRepository.update(id, data);
+  }
+}
