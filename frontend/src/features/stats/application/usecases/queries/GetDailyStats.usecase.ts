7b9368b74c28394138f48609041ee2c65f03e553
coolbeatz71 <sigmacool@gmail.com>
2025-10-17 11:30:00 +0200

    feat: implement stats use cases

---
+++ frontend/src/features/stats/application/usecases/queries/GetDailyStats.usecase.ts
@@ -0,0 +1,21 @@
+import { DailyStatsDto } from '../../../domain/dto/DailyStatsDto';
+import type { IStatsRepository } from '../../repositories/IStatsRepository';
+
+/**
+ * Get daily stats use case for retrieving user creation statistics
+ */
+export class GetDailyStatsUseCase {
+  /**
+   * Creates a new GetDailyStatsUseCase instance
+   * @param {IStatsRepository} statsRepository - Stats repository instance
+   */
+  constructor(private readonly statsRepository: IStatsRepository) {}
+
+  /**
+   * Execute the use case to fetch daily statistics
+   * @returns {Promise<DailyStats[]>} Array of daily statistics
+   */
+  async execute(): Promise<DailyStatsDto[]> {
+    return await this.statsRepository.getDailyStats();
+  }
+}
